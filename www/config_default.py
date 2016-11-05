8b183773d5d3d2fb2a943ac2714c4903f93b2298
muucunn <dhzqzmz@gmail.com>
2016-11-06 07:30:43 +0800

    password='www'改为：password=''  ，user='www'改为：user='root'   #db='awesome'改为db='test'  201611060730

--- www/config_default.py
+++ www/config_default.py
@@ -12,11 +12,16 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www',
-        'password': 'www',
-        'db': 'awesome'
+        'user': 'root',
+        'password': '',
+        'db': 'test'
     },
     'session': {
-        'secret': 'Awesome'
+        'secret': 'Awesome'   #这是什么意思？
     }
 }
+
+'''
+password='www'改为：password=''  ，user='www'改为：user='root' 
+    #db='awesome'改为db='test'  201611060727
+'''
