66cf9573ce02eb1393a461e4b8a58028cc3ce3ea
muucunn <dhzqzmz@gmail.com>
2016-11-08 06:43:18 +0800

    password修改 201611080643

--- www/config_default.py
+++ www/config_default.py
@@ -12,9 +12,9 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www',
-        'password': 'www',
-        'db': 'awesome'
+        'user': '', #www
+        'password': '', #www
+        'db': 'test' #  awesome
     },
     'session': {
         'secret': 'Awesome'
